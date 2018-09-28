5efeb92cef1fb275f36daa9a66ec254f7128cfdf
Patrick Smalen <patricksmalen@MacBook-Pro-van-Patrick.local>
2018-09-28 11:47:06 +0200

    Initial commit

---
+++ src/java/practicum.java
@@ -0,0 +1,30 @@
+package java;
+
+public class practicum
+{
+
+    public static void main(String[] args)
+    {
+
+        boolean patrickAanUit = true;
+
+        String mijnTekst = "Wat leuk!";
+        String nogMeerTekst = "Echt heh! :D";
+
+        String samenTekst = mijnTekst + nogMeerTekst;
+
+        char eenLetter = 'c';
+
+        int getalA = 123;
+        float getalPunt = 1.0f; // Of ook: 1f
+        double getalPuntTwee = 1.0d; // Of ook: 1d
+
+
+        System.out.println( patrickAanUit );
+        System.out.println( samenTekst );
+        System.out.println( getalA + getalPunt + getalPuntTwee );
+
+    }
+
+}
+
